6ac0b370f6c608b948d492097a1cd3e5d226568d
Sebastián Oyarzun <sebaoyarzunruiz@gmail.com>
2022-07-19 03:57:55 -0400

    Update extract_persons.py

--- Work In Progress/extract_persons.py
+++ Work In Progress/extract_persons.py
@@ -1,6 +1,13 @@
 import spacy
 nlp = spacy.load("es_core_news_md")
 
+# MI IDEA
+# Utilizando el cuerpo de una Noticia, se recorre.
+# Se busca todas las palabras que empiezen con mayuscula y esten decorrido. EJ: Los actores Dwayne Johnson y Kevin Hart son...
+# Esas palabra [Los, Dwayn Johnson, Kevin Hart] se guardan en una lista. Se encuentran varias hasta recorrer todo el texto.
+# Luego se busca en la api dewikipedia, si existe y dice que es una persona, se valida.
+# Esto podria entregar una lista de personas mencionadas.
+
 text = """AQUI VAN LOS TEXTOS DE LAS NOTICIAS"""
 
 doc = nlp(text)
